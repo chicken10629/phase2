682820ec86f1337ab6a6df727099010f834e4304
EC2 Default User <ec2-user@ip-172-31-39-48.ap-northeast-1.compute.internal>
2024-11-10 03:01:53 +0000

    [Finish] 4-20

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    #resourceにすると、/:idがURLに含まれなくなる
+    #必要なのはユーザーidとimageidだけなため
+    resources :post_comments, only: [:create, :destroy]
+  end
+  #post_imagesとpost_commentsが親子関係になる
   #Userモデルを作成した時点で以下が作成
   #deviseを使用する際、URLにusersを含む
   devise_for :users
